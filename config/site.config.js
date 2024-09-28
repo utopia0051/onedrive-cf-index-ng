4992ba1ec3853bb60d96b56470fbb313275d682f
utopia0051 <144556266+utopia0051@users.noreply.github.com>
2024-09-28 20:57:33 +0800

    site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -21,7 +21,7 @@ module.exports = {
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 200,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/遵纪守法小组'],
+  protectedRoutes: ['/🚀 遵纪守法小组'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
